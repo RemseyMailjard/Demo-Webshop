a55f15d82d18f6a983d260ed56440bdfaa0455fe
Remsey Mailjard <remsey.mailjard@gmail.com>
2023-10-19 01:15:44 +0200

    test

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+"use strict"; // It is helper code that will check if you have defined a variable. Always the first line of code.
+
+// alert("My Javascript loaded correctly");
+
+//Creating variables
+
